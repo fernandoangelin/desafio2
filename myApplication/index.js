b70794109b21309a50ab8249a2185b6741731148
Fernando Angelin <fernando_angelin@hotmail.com>
2017-10-07 16:40:49 -0300

    Contagem de CPUs feita a partir do javascript. Correção para realizar o instanciamento dos serviços, possibilitando o balanceamento de carga.

--- myApplication/index.js
+++ myApplication/index.js
@@ -1,21 +1,37 @@
 
-//adicionando a biblioteca express
+//criando a variavel cluster, para realizar o balanceamento de carga.
+var cluster = require('cluster');
+
+//criando as variaveis express e app, pedidas pelo desafio
 var express = require('express');
 var app = express();
 
-//recebe o número de processadores via argumento
-numCPU=(process.argv[2]);
+//pega o número de processadores, diz quantos são e diz quantos processos serão lançados.
+numCPU=require('os').cpus().length;
+//console.log('O número de processadores é:', numCPU);
+//console.log('Então, serão lançados', numCPU, 'serviços.');
 
+//teste para saber se o processo é o principal.
+//Caso positivo, ele cria os forks para o número de CPUs encontrados na máquina.
+if(cluster.isMaster) {
+  //laço para criar o fork.
+  for (var i = 0; i < numCPU; i++) {
+    cluster.fork();
+  }
+
+  cluster.on('exit', function(worker, code, signal) {
+    console.log('worker ' + worker.pid + ' died');
+  });
+
+  return;
+}
+
+//função que apresenta a mensagem para quem acessar a porta 3000
 app.get('/', function (req, res) {
 res.send('Hello World!');
 });
 
-console.log('O número de processadores é:', numCPU);
-
-
-//CRIAR UM FORZÃO COM O ARGUMENTO
-
- 
+//porta onde o serviço está ouvindo.
 app.listen(3000, function () {
 console.log('Example app listening on port 3000!');
 });
