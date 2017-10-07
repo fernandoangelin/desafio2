11302e1fa44bd30cbe971627f631ee27eb1005e2
Fernando Angelin <fernando_angelin@hotmail.com>
2017-10-07 16:39:19 -0300

    Adição do comando para iniciar o nodeJS e remoção de comandos shell para contagem de cpus.

--- countAndStart.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-#comando para contar o número de processadores (com threads)
-numCPU=$(nproc)
-
-cd myApplication
-
-#comando para rodar o nodeJS, sendo que as instâncias para cada processador serão instanciadas
-node index.js $numCPU
