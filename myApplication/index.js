066d80b1f1130fcc00a08d9d8bee10e2e7cce538
Fernando Angelin <fernando_angelin@hotmail.com>
2017-10-09 15:36:05 -0300

    Aplicando a cada fork uma porta diferente.

--- myApplication/index.js
+++ myApplication/index.js
@@ -1,39 +1,39 @@
-
+ 
 //criando a variavel cluster, para realizar o balanceamento de carga.
 var cluster = require('cluster');
-
+ 
 //criando a variavel http para receber requisições.
 var http = require('http');
-
+ 
 //criando as variaveis express e app, pedidas pelo desafio
 var express = require('express');
-var app = express();
-
+ 
 //pega o número de processadores, diz quantos são e diz quantos processos serão lançados.
 var numCPU = require('os').cpus().length;
 //console.log('O número de processadores é:', numCPU);
 //console.log('Então, serão lançados', numCPU, 'serviços.');
-
+ 
 //teste para saber se o processo é o principal.
 //Caso positivo, ele cria os forks para o número de CPUs encontrados na máquina.
 if(cluster.isMaster) {
-	//laço para criar o fork.
-	for (var i = 0; i < numCPU; i++) {
-		cluster.fork();
-	}
-	//mensagem para caso o processo é encerrado inesperadamente
-	cluster.on('exit', function(worker, code, signal) {
-		console.log('worker ' + worker.pid + ' died');
-	});
-return;
+    //laço para criar o fork.
+    for (var i = 0; i < numCPU; i++) {
+        cluster.fork({ port: 3000 + i });
+    }
+    //mensagem para caso o processo é encerrado inesperadamente
+    cluster.on('exit', function(worker, code, signal) {
+        console.log('worker ' + worker.pid + ' died');
+    });
+} else {
+    var workerPort = cluster.worker.process.env.port;
+ 
+    var app = express();
+    app.get('/', function (req, res) {
+        //função que apresenta a mensagem para quem acessar a porta 3000
+        res.send('Hello World from port ' + workerPort);
+    });
+ 
+    app.listen(workerPort, function() {
+        console.log('Example app listening on port ' + workerPort);
+    });
 }
-
-//função que apresenta a mensagem para quem acessar a porta 3000
-app.get('/', function (req, res) {
-	res.send('Hello World!');
-});
-
-//porta onde o serviço está ouvindo.
-app.listen(3000, function () {
-	console.log('Example app listening on port 3000!');
-});
