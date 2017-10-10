917926ce294eac135f50c010d0c9b9c65a26ed1d
Fernando Angelin <fernando_angelin@hotmail.com>
2017-10-10 20:25:18 -0300

    Alteração da porta e número de conexões.

--- myApplication/test.js
+++ myApplication/test.js
@@ -2,7 +2,7 @@ const request = require("request");
 
 for(let i = 0; i < 1000; i++) {
 	
-	request.get('http://localhost:3000', function(err, response, body) {
+	request.get('http://localhost:8000', function(err, response, body) {
 		if(err) {
 			console.log(err);
 		} else {
