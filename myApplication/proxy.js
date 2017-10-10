449510e196ab31b435247c6d13ffa0d56a08be02
Fernando Angelin <fernando_angelin@hotmail.com>
2017-10-10 20:24:18 -0300

    Adição do round robin como balanceador de carga dos links criados para cada worker criado no inde

--- myApplication/proxy.js
+++ myApplication/proxy.js
@@ -11,21 +11,25 @@ var apiProxy = httpProxy.createProxyServer();
 var numCPU = require('os').cpus().length;
 
 //criando lista de serviços
-var servers = new Array();
+var addresses = new Array();
 var i;
 
 //porta inicial definida no index.js
 porta=3000;
 
 for (i=0; i<numCPU; i++){
-	servers[i]  = ('http://localhost:'+(porta)).toString();
+	addresses[i]  = ('http://localhost:'+(porta+i)).toString();
 	//porta+i cria para portas diferentes. deixei para apenas uma porta.
 
-	link = servers[i];
+//	link = addresses[i];
+	console.log(addresses[i]);
 
 	app.all('/', function(req, res) {
 			console.log('Redirecionamento ativado!');
+			//implementa um round robin para o balanceamento para links e portas diferentes
+			link=addresses.shift();
 			apiProxy.web(req, res, {target: link});
+			addresses.push(link);
 	});
 }
 app.listen(8000);
