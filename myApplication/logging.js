d93bdf0f30e55528a089c24e583e6bf9e0f2fa4f
Fernando Angelin <fernando_angelin@hotmail.com>
2017-10-11 02:51:16 -0300

    Adicionando para mostrar o logger que poderia ser utilizado.

---
+++ myApplication/logging.js
@@ -0,0 +1,28 @@
+//implementação que cria log
+'use strict';
+const winston = require('winston');
+const fs = require('fs');
+const env = process.env.NODE_ENV || 'development';
+const logDir = 'log';
+// Create the log directory if it does not exist
+if (!fs.existsSync(logDir)) {
+  fs.mkdirSync(logDir);
+}
+const tsFormat = () => (new Date()).toLocaleTimeString();
+const logger = new (winston.Logger)({
+  transports: [
+    // colorize the output to the console
+    new (winston.transports.Console)({
+      timestamp: tsFormat,
+      colorize: true,
+      level: 'info'
+    }),
+    new (require('winston-daily-rotate-file'))({
+      filename: `${logDir}/-web.log`,
+      timestamp: tsFormat,
+      datePattern: 'yyyy-MM-dd',
+      prepend: true,
+      level: env === 'development' ? 'verbose' : 'info'
+    })
+  ]
+});
